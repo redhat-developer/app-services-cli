dfe6593744ed6a2cb31e7d22a2ac49365d96506a
Wojciech Trocki <wtrocki@redhat.com>
2020-10-31 16:37:29 +0000

    fix: support loging flow

--- cmd/login/cmd.go
+++ cmd/login/cmd.go
@@ -3,8 +3,11 @@
 package login
 
 import (
+	"os/exec"
+
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
+	"gitlab.cee.redhat.com/mas-dx/rhmas/cmd/flags"
 )
 
 func NewLoginCommand() *cobra.Command {
@@ -15,9 +18,17 @@ func NewLoginCommand() *cobra.Command {
 		Run:   runLogin,
 	}
 
+	cmd.Flags().String("token", "", "access token that can be used for login")
 	return cmd
 }
 
 func runLogin(cmd *cobra.Command, _ []string) {
-	glog.V(10).Infof("List")
+	token := flags.GetString("token", cmd.Flags())
+	if len(token) > 0 {
+		glog.Infof("Successfully logged in using token")
+	} else {
+		glog.Infof("Redirecting to login page")
+		_ = exec.Command("open", "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/auth?client_id=cloud-services&redirect_uri=https%3A%2F%2Fcloud.redhat.com%2F&state=d8b10b88-8699-4c9b-80fd-665c39343e53&response_mode=fragment&response_type=code&scope=openid&nonce=7ba8050f-5f7b-4a1c-80dd-0392c922f5f8").Start()
+	}
+
 }
