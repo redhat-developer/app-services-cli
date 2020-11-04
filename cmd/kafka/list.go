ec22648b1847f2190e2c3e7543d1ee1dd2a82150
Wojciech Trocki <wtrocki@redhat.com>
2020-11-04 16:54:31 +0000

    fix: list command

--- cmd/kafka/list.go
+++ cmd/kafka/list.go
@@ -14,9 +14,8 @@ import (
 )
 
 const (
-	FlagFormat = "output"
-	FlagPage   = "page"
-	FlagSize   = "size"
+	FlagPage = "page"
+	FlagSize = "size"
 )
 
 var outputFormat string
@@ -30,7 +29,7 @@ func NewListCommand() *cobra.Command {
 		Run:   runList,
 	}
 
-	cmd.Flags().StringVarP(&outputFormat, FlagFormat, "o", "table", "Format to display the Kafka clusters. Choose from \"json\" or \"table\"")
+	cmd.Flags().StringVarP(&outputFormat, "output", "o", "table", "Format to display the Kafka clusters. Choose from \"json\" or \"table\"")
 	cmd.Flags().String(FlagPage, "1", "Page index")
 	cmd.Flags().String(FlagSize, "100", "Number of kafka requests per page")
 
@@ -51,16 +50,15 @@ func runList(cmd *cobra.Command, _ []string) {
 	}
 
 	if status.StatusCode == 200 {
-		displayFormat := flags.GetString(FlagFormat, cmd.Flags())
 		jsonResponse, _ := json.Marshal(response)
 
 		var kafkaList kafka.ClusterList
 		if err = json.Unmarshal(jsonResponse, &kafkaList); err != nil {
 			fmt.Printf("Could not format Kakfa cluster to table: %v", err)
-			displayFormat = "json"
+			outputFormat = "json"
 		}
 
-		switch displayFormat {
+		switch outputFormat {
 		case "json":
 			data, _ := json.MarshalIndent(kafkaList.Items, "", "  ")
 			fmt.Print(string(data))
