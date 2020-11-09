66c9a2c468af75eb66fd16c614ef8d6d995054a6
Wojciech Trocki <wtrocki@redhat.com>
2020-11-09 13:12:50 +0000

    fix: remove function used to test bot

--- test/integration/rhmas_test.go
+++ test/integration/rhmas_test.go
@@ -5,13 +5,6 @@ import (
 	"testing"
 )
 
-func IntMin(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
 func TestIntMinBasic(t *testing.T) {
 	fmt.Println("TODO")
 	t.Skip("Not implemented")
