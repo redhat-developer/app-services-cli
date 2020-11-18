56639f389219e4875bb215345550ee4893bf0673
Wojciech Trocki <wtrocki@redhat.com>
2020-11-18 16:44:36 +0000

    fix: mocked service account api

--- mas-mock/src/handlers.js
+++ mas-mock/src/handlers.js
@@ -1,4 +1,10 @@
 module.exports = {
+  createServiceAccount: async (c, req, res) => {
+    res.status(200).json({
+      clientToken: "1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
+      clientSecret: "1iSY6RQ3JKI8Q0OTmjQFd3ocFRg"
+    });
+  },
   createKafka: async (c, req, res) => {
     res.status(202).json({
       id: "1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
@@ -16,7 +22,7 @@ module.exports = {
       updated_at: "2020-10-05T12:56:36.362208Z",
     });
   },
-  
+
   deleteKafkaById: async (c, req, res) => {
     res.status(204).json({
       id: "1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
@@ -58,32 +64,36 @@ module.exports = {
       page: 1,
       size: 1,
       total: 1,
-      items: [{
-        id: "1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
-        kind: "kafka",
-        href: "/api/managed-services-api/v1/kafkas/1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
-        status: "complete",
-        cloud_provider: "aws",
-        multi_az: false,
-        region: "us-east-1",
-        owner: "api_kafka_service",
-        name: "serviceapi",
-        bootstrapServerHost:
-          "serviceapi-1isy6rq3jki8q0otmjqfd3ocfrg.apps.ms-bttg0jn170hp.x5u8.s1.devshift.org",
-      },
-      {
-        id: "v5Sg6faQ3JKGas4hFd3og45fd31",
-        kind: "kafka",
-        href: "/api/managed-services-api/v1/kafkas/v5Sg6faQ3JKGas4hFd3og45fd31",
-        status: "complete",
-        cloud_provider: "aws",
-        multi_az: false,
-        region: "us-east-1",
-        owner: "api_kafka_service",
-        name: "serviceapi2",
-        bootstrapServerHost:
-          "serviceapi-v5Sg6faQ3JKGas4hFd3og45fd31.apps.ms-dfasf3gsds.23ds.s1.devshift.org",
-      }],
+      items: [
+        {
+          id: "1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
+          kind: "kafka",
+          href:
+            "/api/managed-services-api/v1/kafkas/1iSY6RQ3JKI8Q0OTmjQFd3ocFRg",
+          status: "complete",
+          cloud_provider: "aws",
+          multi_az: false,
+          region: "us-east-1",
+          owner: "api_kafka_service",
+          name: "serviceapi",
+          bootstrapServerHost:
+            "serviceapi-1isy6rq3jki8q0otmjqfd3ocfrg.apps.ms-bttg0jn170hp.x5u8.s1.devshift.org",
+        },
+        {
+          id: "v5Sg6faQ3JKGas4hFd3og45fd31",
+          kind: "kafka",
+          href:
+            "/api/managed-services-api/v1/kafkas/v5Sg6faQ3JKGas4hFd3og45fd31",
+          status: "complete",
+          cloud_provider: "aws",
+          multi_az: false,
+          region: "us-east-1",
+          owner: "api_kafka_service",
+          name: "serviceapi2",
+          bootstrapServerHost:
+            "serviceapi-v5Sg6faQ3JKGas4hFd3og45fd31.apps.ms-dfasf3gsds.23ds.s1.devshift.org",
+        },
+      ],
     });
   },
 
