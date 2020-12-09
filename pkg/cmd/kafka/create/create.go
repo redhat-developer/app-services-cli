35864dc108af4e20decc98410acbd3a2c771609e
Enda Phelan <ephelan@redhat.com>
2020-12-09 12:22:37 +0000

    feat: use kafka cluster after creation

--- pkg/cmd/kafka/create/create.go
+++ pkg/cmd/kafka/create/create.go
@@ -47,8 +47,7 @@ func NewCreateCommand() *cobra.Command {
 			}
 			opts.cfg = cfg
 
-			err = kafka.ValidateName(opts.name)
-			if err != nil {
+			if err = kafka.ValidateName(opts.name); err != nil {
 				return err
 			}
 
@@ -59,8 +58,6 @@ func NewCreateCommand() *cobra.Command {
 	cmd.Flags().StringVarP(&opts.name, flags.FlagName, "n", "", "Name of the new Kafka cluster")
 	cmd.Flags().StringVar(&opts.provider, flags.FlagProvider, "aws", "Cloud provider ID")
 	cmd.Flags().StringVar(&opts.region, flags.FlagRegion, "us-east-1", "Cloud Provider Region ID")
-	// Hardcoded as only true is possible
-	// cmd.Flags().BoolVar(&opts.multiAZ, flags.FlagMultiAZ, true, "Determines if cluster should be provisioned across multiple Availability Zones")
 	cmd.Flags().StringVarP(&opts.outputFormat, "output", "o", "json", "Format to display the Kafka cluster. Choose from: \"json\", \"yaml\", \"yml\"")
 
 	_ = cmd.MarkFlagRequired(flags.FlagName)
@@ -69,12 +66,16 @@ func NewCreateCommand() *cobra.Command {
 }
 
 func runCreate(opts *options) error {
-	connection, err := opts.cfg.Connection()
+	cfg := opts.cfg
+
+	connection, err := cfg.Connection()
 	if err != nil {
 		return fmt.Errorf("Can't create connection: %w", err)
 	}
 
 	client := connection.NewMASClient()
+
+	fmt.Fprintln(os.Stderr, "Creating Kafka cluster")
 
 	kafkaRequest := managedservices.KafkaRequestPayload{Name: opts.name, Region: opts.region, CloudProvider: opts.provider, MultiAz: true}
 	response, _, err := client.DefaultApi.CreateKafka(context.Background(), true, kafkaRequest)
@@ -83,15 +84,26 @@ func runCreate(opts *options) error {
 		return fmt.Errorf("Error while requesting new Kafka cluster: %w", err)
 	}
 
-	fmt.Fprintf(os.Stderr, "Created new Kafka cluster:\n")
+	fmt.Fprintln(os.Stderr, "Created Kafka cluster:")
 
 	switch opts.outputFormat {
 	case "json":
 		data, _ := json.MarshalIndent(response, "", cmdutil.DefaultJSONIndent)
-		fmt.Print(string(data))
+		fmt.Println(string(data))
 	case "yaml", "yml":
 		data, _ := yaml.Marshal(response)
-		fmt.Print(string(data))
+		fmt.Println(string(data))
+	}
+
+	kafkaCfg := &config.KafkaConfig{
+		ClusterName: response.Name,
+		ClusterHost: response.BootstrapServerHost,
+		ClusterID:   response.Id,
+	}
+
+	cfg.Services.SetKafka(kafkaCfg)
+	if err := config.Save(cfg); err != nil {
+		return fmt.Errorf("Unable to automatically use Kafka cluster: %w", err)
 	}
 
 	return nil
