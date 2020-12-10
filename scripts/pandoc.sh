636db80b5ca8beeddc806f846c74c6776b1ee356
Wojciech Trocki <wtrocki@redhat.com>
2020-12-10 12:21:28 +0000

    fix: navigation for cli documentation

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -1,10 +1,14 @@
 files=($(find "./docs/commands" -type f -name '*.md'))
+echo "Generating  adoc"
 for item in ${files[*]}
 do
   filename="`basename ${item%.*}`"   
   adocFile="${item%.*}.adoc"   
   printf "Working on   %s\n" $filename
-  pandoc -s $item -f markdown -t asciidoc -o ${DIR}${adocFile}
+  pandoc -s $item -f markdown -t asciidoc -o ${adocFile}
+
+  sed -i '' 's/.md/.adoc/g' ${adocFile}
   echo "Generated adoc. Removing markdown"
+  rm -f ${item}
   rm -f ${item}
 done
