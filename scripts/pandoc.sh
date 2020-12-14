ebb14126c88c21a089018b465eddb026b7d734f0
Enda Phelan <ephelan@redhat.com>
2020-12-14 12:42:59 +0000

    chore: fix pandoc script

--- scripts/pandoc.sh
+++ scripts/pandoc.sh
@@ -1,5 +1,11 @@
+if ! command -v pandoc &> /dev/null
+then
+  echo "Please install pandoc [https://pandoc.org]"
+  exit 1
+fi
+
 files=($(find "./docs/commands" -type f -name '*.md'))
-echo "Generating  adoc"
+echo "Generating adoc"
 for item in ${files[*]}
 do
   filename="`basename ${item%.*}`"   
@@ -7,7 +13,12 @@ do
   printf "Working on   %s\n" $filename
   pandoc -s $item -f markdown -t asciidoc -o ${adocFile}
 
-  sed -i '' 's/.md/.adoc/g' ${adocFile}
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sed -i 's/.md/.adoc/g' ${adocFile}
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's/.md/.adoc/g' ${adocFile}
+  fi
+
   echo "Generated adoc. Removing markdown"
   rm -f ${item}
 done
