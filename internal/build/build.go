4b2d3b36151b104c802cd27f9265210aeee25fe8
Enda <ephelan@redhat.com>
2021-01-05 14:07:49 +0000

    chore: unique build versions for development (#195)

---
+++ internal/build/build.go
@@ -0,0 +1,16 @@
+package build
+
+import (
+	"runtime/debug"
+)
+
+// Version is dynamically set by the toolchain or overridden by the Makefile.
+var Version = "dev"
+
+func init() {
+	if Version == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "(devel)" {
+			Version = info.Main.Version
+		}
+	}
+}
