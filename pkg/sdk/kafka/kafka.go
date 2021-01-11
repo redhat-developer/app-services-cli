acb62a2db541614683ae4187f725534080c4b1e5
Enda Phelan <ephelan@redhat.com>
2021-01-11 16:14:49 +0000

    test: add tests

--- pkg/sdk/kafka/kafka.go
+++ pkg/sdk/kafka/kafka.go
@@ -40,11 +40,17 @@ func ValidateName(name string) error {
 
 // TransformResponse modifies fields from the KafkaRequest payload object
 // The main transformation is appending ":443" to the Bootstrap Server URL
-func TransformResponse(kafkaInstance *managedservices.KafkaRequest) {
+func TransformResponse(kafkaInstance *managedservices.KafkaRequest) *managedservices.KafkaRequest {
 	bootstrapHost := kafkaInstance.GetBootstrapServerHost()
+
+	if bootstrapHost == "" {
+		return kafkaInstance
+	}
 
 	if !strings.HasSuffix(bootstrapHost, ":443") {
 		hostURL := fmt.Sprintf("%v:443", bootstrapHost)
 		kafkaInstance.SetBootstrapServerHost(hostURL)
 	}
+
+	return kafkaInstance
 }
