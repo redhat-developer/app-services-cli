263cacd82b0796c1d198f669a39bc9b87f385036
Enda Phelan <ephelan@redhat.com>
2021-01-19 16:45:49 +0000

    chore(scripts): add script to delete service accounts

---
+++ scripts/util/delete-service-accounts.sh
@@ -0,0 +1,14 @@
+if [ -z "$1" ]; then
+  echo "You cannot delete all service accounts. Pass an argument to filter on the name"
+  exit 1
+fi
+
+echo "Deleting service accounts which include '$1' in the name"
+
+serviceaccounts=$(rhoas serviceaccount list -o json | jq -r -c --arg filter "$1" '.items[] | select(.name | contains($filter))')
+
+for sa in ${serviceaccounts}; do
+  name=$(echo $sa | jq -r '.name')
+  echo "Deleting '$name'..."
+  rhoas serviceaccount delete --id $(echo $sa | jq -r '.id')
+done
