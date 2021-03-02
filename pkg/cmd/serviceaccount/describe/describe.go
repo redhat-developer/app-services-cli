da63580f1675ce125c9b641cf7e59128e10365cf
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-03-02 17:19:33 +0530

    feat(serviceaccount describe): add describe command (#406)

---
+++ pkg/cmd/serviceaccount/describe/describe.go
@@ -0,0 +1,86 @@
+package describe
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/bf2fc6cc711aee1a0c2a/cli/internal/config"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/internal/localizer"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/pkg/cmd/factory"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/pkg/cmd/flag"
+	flagutil "github.com/bf2fc6cc711aee1a0c2a/cli/pkg/cmdutil/flags"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/pkg/connection"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/pkg/dump"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/pkg/iostreams"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+type Options struct {
+	id           string
+	outputFormat string
+
+	IO         *iostreams.IOStreams
+	Config     config.IConfig
+	Connection func() (connection.Connection, error)
+}
+
+func NewDescribeCommand(f *factory.Factory) *cobra.Command {
+	opts := &Options{
+		Config:     f.Config,
+		Connection: f.Connection,
+		IO:         f.IOStreams,
+	}
+
+	cmd := &cobra.Command{
+		Use:     localizer.MustLocalizeFromID("serviceAccount.describe.cmd.use"),
+		Short:   localizer.MustLocalizeFromID("serviceAccount.describe.cmd.shortDescription"),
+		Long:    localizer.MustLocalizeFromID("serviceAccount.describe.cmd.longDescription"),
+		Example: localizer.MustLocalizeFromID("serviceAccount.describe.cmd.example"),
+		Args:    cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			validOutputFormats := flagutil.ValidOutputFormats
+			if opts.outputFormat != "" && !flagutil.IsValidInput(opts.outputFormat, validOutputFormats...) {
+				return flag.InvalidValueError("output", opts.outputFormat, validOutputFormats...)
+			}
+
+			return runDescribe(opts)
+		},
+	}
+
+	cmd.Flags().StringVar(&opts.id, "id", "", localizer.MustLocalizeFromID("serviceAccount.describe.flag.id.description"))
+	cmd.Flags().StringVarP(&opts.outputFormat, "output", "o", "json", localizer.MustLocalizeFromID("serviceAccount.common.flag.output.description"))
+
+	_ = cmd.MarkFlagRequired("id")
+
+	return cmd
+}
+
+func runDescribe(opts *Options) error {
+
+	connection, err := opts.Connection()
+	if err != nil {
+		return err
+	}
+
+	api := connection.API()
+
+	a := api.Kafka().GetServiceAccountById(context.Background(), opts.id)
+	res, _, apiErr := a.Execute()
+
+	if apiErr.Error() != "" {
+		return fmt.Errorf("%v: %w", localizer.MustLocalizeFromID("serviceAccount.describe.error.unableToDescribe"), apiErr)
+	}
+
+	switch opts.outputFormat {
+	case "yaml", "yml":
+		data, _ := yaml.Marshal(res)
+		_ = dump.YAML(opts.IO.Out, data)
+	default:
+		data, _ := json.Marshal(res)
+		_ = dump.JSON(opts.IO.Out, data)
+	}
+
+	return nil
+}
