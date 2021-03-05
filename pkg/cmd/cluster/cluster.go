00d98ad292cbd8836bc31cb3d6b1356d2b9bdf98
Wojtek Trocki <wtrocki@redhat.com>
2021-03-05 10:56:22 +0000

    fix: make binding executable directly in the bash (#419)

--- pkg/cmd/cluster/cluster.go
+++ pkg/cmd/cluster/cluster.go
@@ -11,9 +11,10 @@ import (
 // NewServiceAccountCommand creates a new command sub-group to manage service accounts
 func NewClusterCommand(f *factory.Factory) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   localizer.MustLocalizeFromID("cluster.cmd.use"),
-		Short: localizer.MustLocalizeFromID("cluster.cmd.shortDescription"),
-		Args:  cobra.ExactArgs(1),
+		Use:     localizer.MustLocalizeFromID("cluster.cmd.use"),
+		Short:   localizer.MustLocalizeFromID("cluster.cmd.shortDescription"),
+		Example: localizer.MustLocalizeFromID("cluster.cmd.example"),
+		Args:    cobra.ExactArgs(1),
 	}
 
 	cmd.AddCommand(
