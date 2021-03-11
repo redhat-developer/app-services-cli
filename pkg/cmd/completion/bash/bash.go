0585823101264718e1783b2e583ba737877ac573
Enda <ephelan@redhat.com>
2021-03-11 17:18:53 +0000

    refactor: completion command (#428)

---
+++ pkg/cmd/completion/bash/bash.go
@@ -0,0 +1,21 @@
+package bash
+
+import (
+	"github.com/bf2fc6cc711aee1a0c2a/cli/internal/localizer"
+	"github.com/bf2fc6cc711aee1a0c2a/cli/pkg/cmd/factory"
+	"github.com/spf13/cobra"
+)
+
+func NewCommand(f *factory.Factory) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                   localizer.MustLocalizeFromID("completion.bash.cmd.use"),
+		Short:                 localizer.MustLocalizeFromID("completion.bash.cmd.shortDescription"),
+		Long:                  localizer.MustLocalizeFromID("completion.bash.cmd.longDescription"),
+		DisableFlagsInUseLine: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return cmd.Parent().Parent().GenBashCompletion(f.IOStreams.Out)
+		},
+	}
+
+	return cmd
+}
