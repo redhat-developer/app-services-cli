f110ad0fcfd3ee74d21455c2b4e51ff37fc7e9ff
Enda Phelan <ephelan@redhat.com>
2021-03-26 11:28:15 +0000

    fix(config): check if .config directory exists
    This checks if the directory exists and if it does not uses $HOME

--- internal/config/util.go
+++ internal/config/util.go
@@ -18,10 +18,7 @@ func getUserConfig(name string) (string, error) {
 	case "darwin":
 		return filepath.Join(os.Getenv("HOME"), "Library", "Application Support", name), nil
 	case "linux":
-		baseDir := os.Getenv("XDG_CONFIG_HOME")
-		if baseDir == "" {
-			baseDir = filepath.Join(os.Getenv("HOME"), ".config")
-		}
+		baseDir := getLinuxConfigBaseDir()
 		return filepath.Join(baseDir, name), nil
 	default:
 		return "", errors.New(localizer.MustLocalize(&localizer.Config{
@@ -31,4 +28,19 @@ func getUserConfig(name string) (string, error) {
 			},
 		}))
 	}
+}
+
+// Gets the config directory for Linux
+func getLinuxConfigBaseDir() string {
+	baseDir := os.Getenv("XDG_CONFIG_HOME")
+	if baseDir != "" {
+		return baseDir
+	}
+	baseDir = filepath.Join(os.Getenv("HOME"), ".config")
+	// The config directory does not exist on the current environment
+	// use the HOME directory
+	if _, err := os.Stat(baseDir); os.IsNotExist(err) {
+		baseDir = os.Getenv("HOME")
+	}
+	return baseDir
 }
