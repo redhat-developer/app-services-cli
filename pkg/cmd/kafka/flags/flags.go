bf11f5f7343e2fc4d868f0bdd8199430551e5790
Wojciech Trocki <wtrocki@redhat.com>
2021-06-24 12:58:20 +0100

    chore: Clean up flags in CLI (#747)
    * chore: remove unused file
    
    * chore: remove unused flags in file

--- pkg/cmd/kafka/flags/flags.go
+++ pkg/cmd/kafka/flags/flags.go
@@ -1,18 +1,8 @@
 package flags
 
 const (
-	// FlagID is a flag representing a Kafka ID
-	FlagID = "id"
-	// FlagName is a flag representing a Kafka name
-	FlagName = "name"
-	// FlagOwner is a flag representing a Kafka owner name
-	FlagOwner = "owner"
 	// FlagProvider is a flag representing an OCM provider ID
 	FlagProvider = "provider"
 	// FlagRegion is a flag representing an OCM region ID
 	FlagRegion = "region"
-	// FlagMultiAZ is a flag representing a whether a Kafka is multi AZ
-	FlagMultiAZ = "multi-az"
-	// FlagClusterID is a flag representing a Kafka's cluster ID
-	FlagClusterID = "cluster-id"
 )
--- pkg/cmd/kafka/topic/flags/flags.go
+++
@@ -1,6 +0,0 @@
-package flags
-
-const (
-	// FlagName is a flag representing a topic name
-	FlagName = "name"
-)
