55f406996ea3c5b58e30232501d3ceb8681fbf19
Wojciech Trocki <wtrocki@redhat.com>
2021-06-30 17:00:07 +0100

    chore: mocked config

--- pkg/profile/profile_test.go
+++ pkg/profile/profile_test.go
@@ -3,12 +3,16 @@ package profile
 import (
 	"testing"
 
+	"github.com/redhat-developer/app-services-cli/internal/config"
+	"github.com/redhat-developer/app-services-cli/internal/mockutil"
 	"github.com/redhat-developer/app-services-cli/pkg/cmd/factory"
 )
 
 func TestEnableDevPreviewConfig(t *testing.T) {
 	testVal := ""
-	config, err := EnableDevPreview(factory.New("dev", nil), testVal)
+	factoryObj := factory.New("dev", nil)
+	factoryObj.Config = mockutil.NewConfigMock(&config.Config{})
+	config, err := EnableDevPreview(factoryObj, testVal)
 	if config != nil {
 		t.Errorf("TestEnableDevPreviewConfig config = %v, want %v", config, nil)
 	}
@@ -17,7 +21,7 @@ func TestEnableDevPreviewConfig(t *testing.T) {
 	}
 
 	testVal = "giberish"
-	config, err = EnableDevPreview(factory.New("dev", nil), testVal)
+	config, err = EnableDevPreview(factoryObj, testVal)
 	if config.DevPreviewEnabled == true {
 		t.Errorf("TestEnableDevPreviewConfig() config.DevPreviewEnabled = %v, want %v", config.DevPreviewEnabled, false)
 	}
@@ -26,7 +30,7 @@ func TestEnableDevPreviewConfig(t *testing.T) {
 	}
 
 	testVal = "true"
-	config, err = EnableDevPreview(factory.New("dev", nil), testVal)
+	config, err = EnableDevPreview(factoryObj, testVal)
 	if config.DevPreviewEnabled != true {
 		t.Errorf("TestEnableDevPreviewConfig config.DevPreviewEnabled = %v, want %v", config.DevPreviewEnabled, true)
 	}
@@ -35,7 +39,7 @@ func TestEnableDevPreviewConfig(t *testing.T) {
 	}
 
 	testVal = "yes"
-	config, err = EnableDevPreview(factory.New("dev", nil), testVal)
+	config, err = EnableDevPreview(factoryObj, testVal)
 	if config.DevPreviewEnabled != true {
 		t.Errorf("TestEnableDevPreviewConfig config.DevPreviewEnabled = %v, want %v", config.DevPreviewEnabled, true)
 	}
@@ -45,20 +49,21 @@ func TestEnableDevPreviewConfig(t *testing.T) {
 }
 
 func TestDevPreviewEnabled(t *testing.T) {
-	f := factory.New("dev", nil)
+	factoryObj := factory.New("dev", nil)
+	factoryObj.Config = mockutil.NewConfigMock(&config.Config{})
 	testVal := "false"
-	config, _ := EnableDevPreview(f, testVal)
+	config, _ := EnableDevPreview(factoryObj, testVal)
 	t.Log("Ess", config)
-	enabled := DevPreviewEnabled(f)
+	enabled := DevPreviewEnabled(factoryObj)
 
 	if enabled {
 		t.Errorf("TestEnableDevPreviewConfig enabled = %v, want %v", enabled, false)
 	}
 
 	testVal = "true"
-	_, _ = EnableDevPreview(f, testVal)
+	_, _ = EnableDevPreview(factoryObj, testVal)
 
-	enabled = DevPreviewEnabled(f)
+	enabled = DevPreviewEnabled(factoryObj)
 	if !enabled {
 		t.Errorf("TestEnableDevPreviewConfig enabled = %v, want %v", enabled, true)
 	}
