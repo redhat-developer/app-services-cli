c2362ce040d8be012b642ff0423e2e5a8d91d40e
Enda <ephelan@redhat.com>
2021-07-28 17:08:40 +0100

    feat: rename debug flag to verbose (#845)
    BREAKING: The `debug` flag is now `verbose`. There is no longer any shorthand for `version`.

--- pkg/cmd/debug/flag.go
+++ pkg/cmd/debug/flag.go
@@ -4,21 +4,21 @@ package debug
 
 import "github.com/spf13/pflag"
 
-// AddFlag adds the debug flag to the given set of command line flags.
+// AddFlag adds the verbose flag to the given set of command line flags.
 func AddFlag(flags *pflag.FlagSet) {
 	flags.BoolVarP(
 		&enabled,
-		"debug",
-		"d",
+		"verbose",
+		"v",
 		false,
-		"Enable debug mode",
+		"Enable verbose mode",
 	)
 }
 
-// Enabled returns a boolean flag that indicates if the debug mode is enabled
+// Enabled returns a boolean flag that indicates if the verbose mode is enabled
 func Enabled() bool {
 	return enabled
 }
 
-// enabled is a boolean flag that indicates that the debug mode is enabled
+// enabled is a boolean flag that indicates that the verbose mode is enabled
 var enabled bool
