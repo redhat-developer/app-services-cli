dc1ec20a61b1dd1359c67f42646662c2d915e5b8
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-08-09 20:06:22 +0530

    fix: use different branch of SDK

--- pkg/cmd/kafka/consumergroup/resetoffset/reset_offset.go
+++ pkg/cmd/kafka/consumergroup/resetoffset/reset_offset.go
@@ -2,8 +2,8 @@ package resetoffset
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -13,11 +13,13 @@ import (
 	"github.com/redhat-developer/app-services-cli/pkg/cmdutil"
 	flagutil "github.com/redhat-developer/app-services-cli/pkg/cmdutil/flags"
 	"github.com/redhat-developer/app-services-cli/pkg/connection"
+	"github.com/redhat-developer/app-services-cli/pkg/dump"
 	"github.com/redhat-developer/app-services-cli/pkg/iostreams"
 	"github.com/redhat-developer/app-services-cli/pkg/localize"
 	"github.com/redhat-developer/app-services-cli/pkg/logging"
 	kafkainstanceclient "github.com/redhat-developer/app-services-sdk-go/kafkainstance/apiv1internal/client"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 type Options struct {
@@ -28,12 +30,19 @@ type Options struct {
 	offset      string
 	topic       string
 	partitions  []int32
+	output      string
 
 	IO         *iostreams.IOStreams
 	Config     config.IConfig
 	Connection factory.ConnectionFunc
 	Logger     func() (logging.Logger, error)
 	localizer  localize.Localizer
+}
+
+type UpdatedConsumerRow struct {
+	Topic     string `json:"groupId,omitempty" header:"Topic"`
+	Partition int32  `json:"active_members,omitempty" header:"Partition"`
+	Offset    int32  `json:"lag,omitempty" header:"Offset"`
 }
 
 // NewResetOffsetConsumerGroupCommand gets a new command for resetting offset for a consumer group.
@@ -53,6 +62,10 @@ func NewResetOffsetConsumerGroupCommand(f *factory.Factory) *cobra.Command {
 		Example: opts.localizer.MustLocalize("kafka.consumerGroup.resetOffset.cmd.example"),
 		Args:    cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			if opts.output != "" && !flagutil.IsValidInput(opts.output, flagutil.ValidOutputFormats...) {
+				return flag.InvalidValueError("output", opts.output, flagutil.ValidOutputFormats...)
+			}
 
 			if opts.offset != "" {
 				if err = flag.ValidateOffset(opts.offset); err != nil {
@@ -85,9 +98,11 @@ func NewResetOffsetConsumerGroupCommand(f *factory.Factory) *cobra.Command {
 	cmd.Flags().StringVar(&opts.offset, "offset", "", opts.localizer.MustLocalize("kafka.consumerGroup.resetOffset.flag.offset"))
 	cmd.Flags().StringVar(&opts.topic, "topic", "", opts.localizer.MustLocalize("kafka.consumerGroup.resetOffset.flag.topic"))
 	cmd.Flags().Int32SliceVar(&opts.partitions, "partitions", []int32{}, opts.localizer.MustLocalize("kafka.consumerGroup.resetOffset.flag.partitions"))
+	cmd.Flags().StringVarP(&opts.output, "output", "o", "", opts.localizer.MustLocalize("kafka.consumerGroup.resetOffset.flag.output"))
 
 	_ = cmd.MarkFlagRequired("id")
 	_ = cmd.MarkFlagRequired("offset")
+	_ = cmd.MarkFlagRequired("topic")
 
 	// flag based completions for ID
 	_ = cmd.RegisterFlagCompletionFunc("id", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
@@ -99,6 +114,7 @@ func NewResetOffsetConsumerGroupCommand(f *factory.Factory) *cobra.Command {
 		return cmdutil.FilterValidTopicNameArgs(f, toComplete)
 	})
 
+	flagutil.EnableOutputFlagCompletion(cmd)
 	flagutil.EnableStaticFlagCompletion(cmd, "offset", flagutil.ValidOffsets)
 
 	return cmd
@@ -166,8 +182,6 @@ func runCmd(opts *Options) error {
 	}
 
 	updatedConsumers, httpRes, err := a.Execute()
-	// display the response object(todo)
-	fmt.Println(updatedConsumers)
 
 	if err != nil {
 
@@ -193,6 +207,40 @@ func runCmd(opts *Options) error {
 
 	defer httpRes.Body.Close()
 
+	logger.Info(opts.localizer.MustLocalize("kafka.consumerGroup.resetOffset.log.info.successful", localize.NewEntry("ConsumerGroupID", opts.id), localize.NewEntry("InstanceName", kafkaInstance.GetName())))
+
+	switch opts.output {
+	case "json":
+		data, _ := json.Marshal(updatedConsumers)
+		_ = dump.JSON(opts.IO.Out, data)
+	case "yaml", "yml":
+		data, _ := yaml.Marshal(updatedConsumers)
+		_ = dump.YAML(opts.IO.Out, data)
+	default:
+		logger.Info("")
+		consumers := updatedConsumers.GetItems()
+		rows := mapResetOffsetResultToTableFormat(consumers)
+		dump.Table(opts.IO.Out, rows)
+
+		return nil
+	}
+
 	return nil
 
+}
+
+func mapResetOffsetResultToTableFormat(consumers []kafkainstanceclient.ConsumerGroupResetOffsetResultItem) []UpdatedConsumerRow {
+	rows := []UpdatedConsumerRow{}
+
+	for _, t := range consumers {
+
+		row := UpdatedConsumerRow{
+			Topic:     t.GetTopic(),
+			Partition: t.GetPartition(),
+			Offset:    t.GetOffset(),
+		}
+		rows = append(rows, row)
+	}
+
+	return rows
 }
