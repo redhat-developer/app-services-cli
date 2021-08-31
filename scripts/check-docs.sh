6e446b68d731c9b977a872e867cc6aec0a20719a
Alex Rodin <alexander.a.rodin@gmail.com>
2021-08-31 13:32:43 -0400

    fix: enable debug in check-docs.sh

--- scripts/check-docs.sh
+++ scripts/check-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -uox pipefail
 commands=$(git diff --stat docs/commands | wc -l)
 if [[ $(($commands)) != 0 ]]; then
   echo "./docs/commands has changes"
