1ded8cf5b31df5dffc647c319ca7ecd84004fed0
Alex Rodin <alexander.a.rodin@gmail.com>
2021-08-31 13:39:15 -0400

    fix: print the changes

--- scripts/check-docs.sh
+++ scripts/check-docs.sh
@@ -3,6 +3,7 @@
 set -uo pipefail
 commands=$(git diff --stat docs/commands | wc -l)
 if [[ $(($commands)) != 0 ]]; then
-  echo "./docs/commands directory has " + $(($commands)) +" changes"
+  echo "./docs/commands directory has " $(($commands)) " changes"
+  git diff --stat docs/commands
   exit 1
 fi
