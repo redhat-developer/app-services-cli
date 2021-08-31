3ad5f0546a10738341f71efde29ec75e9d94aab6
Alex Rodin <alexander.a.rodin@gmail.com>
2021-08-31 13:14:50 -0400

    fix: fixed the problem with integer comparison in check-docs.sh

--- scripts/check-docs.sh
+++ scripts/check-docs.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -uo pipefail
-
-if [[ $(git diff --stat docs/commands | wc -l) != 0 ]]; then
+commands=$(git diff --stat docs/commands | wc -l)
+if [[ $(($commands)) != 0 ]]; then
   echo "./docs/commands has changes"
   exit 1
 fi
