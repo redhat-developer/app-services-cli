f6b61c2dc7903452db539ba5c7d40e3137b2612a
Enda Phelan <ephelan@redhat.com>
2021-09-24 15:36:09 +0100

    fix: set --role flag as required

--- pkg/cmd/registry/artifact/role/add/add.go
+++ pkg/cmd/registry/artifact/role/add/add.go
@@ -99,6 +99,7 @@ func NewAddCommand(f *factory.Factory) *cobra.Command {
 	cmd.Flags().StringVar(&opts.role, "role", "", opts.localizer.MustLocalize("registry.role.cmd.flag.role.description"))
 	cmd.Flags().StringVar(&opts.registryID, "instance-id", "", opts.localizer.MustLocalize("artifact.common.instance.id"))
 
+	_ = cmd.MarkFlagRequired("role")
 	_ = cmd.RegisterFlagCompletionFunc("role", func(cmd *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 		return util.AllowedRoleTypeEnumValues, cobra.ShellCompDirectiveNoSpace
 	})
