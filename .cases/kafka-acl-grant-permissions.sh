3928588608aa142f0a9d39c2436d7c6f84af4f5c
Wojciech Trocki <wtrocki@redhat.com>
2021-10-14 12:54:57 +0100

    fix!: rename grant permissions command

--- .cases/kafka-acl-grant-permissions.sh
+++ .cases/kafka-acl-grant-permissions.sh
@@ -13,47 +13,47 @@
 alias rhoas=$(go env GOPATH)/bin/rhoas
 
 ## Cases
-rhoas kafka acl grant-permissions 
+rhoas kafka acl grant-access 
 
 
-rhoas kafka acl grant-permissions --producer
+rhoas kafka acl grant-access --producer
 
 
-rhoas kafka acl grant-permissions --producer --consumer
+rhoas kafka acl grant-access --producer --consumer
 
 
-rhoas kafka acl grant-permissions --producer --service-account test_case --user test_case
+rhoas kafka acl grant-access --producer --service-account test_case --user test_case
 
 
-rhoas kafka acl grant-permissions --consumer --service-account test_case --topic test_case --group test_case --group-prefix test_case
+rhoas kafka acl grant-access --consumer --service-account test_case --topic test_case --group test_case --group-prefix test_case
 
 
-rhoas kafka acl grant-permissions --producer --service-account test_case --topic test_case --topic-prefix test_case
+rhoas kafka acl grant-access --producer --service-account test_case --topic test_case --topic-prefix test_case
 
 
 ## Cases that require cleanup 
 
-rhoas kafka acl grant-permissions --producer --service-account test_case --topic test_case 
+rhoas kafka acl grant-access --producer --service-account test_case --topic test_case 
 
 
-rhoas kafka acl grant-permissions --producer --service-account test_prefix --topic-prefix test_ 
+rhoas kafka acl grant-access --producer --service-account test_prefix --topic-prefix test_ 
 
 
-rhoas kafka acl grant-permissions --producer --user test_user --topic-prefix test_ 
+rhoas kafka acl grant-access --producer --user test_user --topic-prefix test_ 
 
 
-rhoas kafka acl grant-permissions --producer --user test_user --topic test_topic
+rhoas kafka acl grant-access --producer --user test_user --topic test_topic
 
-rhoas kafka acl grant-permissions --producer --user all --topic all
+rhoas kafka acl grant-access --producer --user all --topic all
 
-rhoas kafka acl grant-permissions --consumer --service-account test_prefix --topic-prefix test_ 
-
-
-rhoas kafka acl grant-permissions --consumer --service-account test_consumer --topic-prefix test_  --group test_group
+rhoas kafka acl grant-access --consumer --service-account test_prefix --topic-prefix test_ 
 
 
-rhoas kafka acl grant-permissions --consumer --service-account test_prefix --topic-prefix test_  --group-prefix test_
+rhoas kafka acl grant-access --consumer --service-account test_consumer --topic-prefix test_  --group test_group
 
 
-rhoas kafka acl grant-permissions --consumer --producer --service-account consumer-producer --topic-prefix test_  --group-prefix test_
+rhoas kafka acl grant-access --consumer --service-account test_prefix --topic-prefix test_  --group-prefix test_
+
+
+rhoas kafka acl grant-access --consumer --producer --service-account consumer-producer --topic-prefix test_  --group-prefix test_
 
