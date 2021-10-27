778cfb6c21269b837fd67812028375d62337ed84
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2021-10-27 19:01:08 +0530

    fix: address review comments

--- pkg/cmd/kafka/acl/admin/admin.go
+++ pkg/cmd/kafka/acl/admin/admin.go
@@ -138,6 +138,9 @@ func runAdmin(opts *options) (err error) {
 		kafkainstanceclient.ACLPERMISSIONTYPE_ALLOW,
 	)
 
+	opts.logger.Info(opts.localizer.MustLocalize("kafka.acl.grantPermissions.log.info.aclsPreview"))
+	opts.logger.Info()
+
 	rows := aclutil.MapACLsToTableRows([]kafkainstanceclient.AclBinding{*aclBindClusterAlter}, opts.localizer)
 	dump.Table(opts.io.Out, rows)
 	opts.logger.Info()
