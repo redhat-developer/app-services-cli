0d67a09b0252f9e3d3f2d41f9df5e1ceda8cb999
Enda Phelan <ephelan@redhat.com>
2021-11-09 16:35:40 +0000

    fix: version check was not working because of differences in Git tags

--- internal/build/build.go
+++ internal/build/build.go
@@ -3,6 +3,7 @@ package build
 import (
 	"context"
 	"runtime/debug"
+	"strings"
 	"time"
 
 	"github.com/google/go-github/v39/github"
@@ -76,9 +77,13 @@ func init() {
 // CheckForUpdate checks if there is a newer version of the CLI than
 // the version currently being used. If so, it logs this information
 // to the console.
-func CheckForUpdate(ctx context.Context, logger logging.Logger, localizer localize.Localizer) {
+func CheckForUpdate(ctx context.Context, version string, logger logging.Logger, localizer localize.Localizer) {
 	if BuildSource != string(githubBuildSource) {
 		return
+	}
+	// prefix version with a v to correspond with Git tag
+	if !strings.HasPrefix(version, "v") {
+		version = "v" + version
 	}
 
 	releases, err := getReleases(ctx)
@@ -97,12 +102,12 @@ func CheckForUpdate(ctx context.Context, logger logging.Logger, localizer locali
 		// create an tag:index map of the releases
 		// the first index (0) is the latest release
 		releaseTagIndexMap[release.GetTagName()] = i
-		if release.GetTagName() == Version {
+		if release.GetTagName() == version {
 			break
 		}
 	}
 
-	currentVersionIndex, ok := releaseTagIndexMap[Version]
+	currentVersionIndex, ok := releaseTagIndexMap[version]
 	if !ok {
 		// the currently used version does not exist as a public release
 		// assume it to be an unpublished or dev release
