be5291ce8a7f93b8416d39d3fc8c918e0fcd429c
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2022-02-14 16:17:48 +0530

    refactor: removed unused error handlers

--- pkg/cmd/registry/rule/rulecmdutil/error.go
+++ pkg/cmd/registry/rule/rulecmdutil/error.go
@@ -13,18 +13,6 @@ func (r *RegistryRuleError) ConflictError(ruleType string) error {
 	return r.Localizer.MustLocalizeError("registry.rule.common.error.conflict", localize.NewEntry("Type", ruleType))
 }
 
-func (r *RegistryRuleError) UnathorizedError(operation string) error {
-	return r.Localizer.MustLocalizeError("registry.rule.common.error.unauthorized", localize.NewEntry("Operation", operation))
-}
-
-func (r *RegistryRuleError) ForbiddenError(operation string) error {
-	return r.Localizer.MustLocalizeError("registry.rule.common.error.forbidden", localize.NewEntry("Operation", operation))
-}
-
-func (r *RegistryRuleError) ServerError() error {
-	return r.Localizer.MustLocalizeError("registry.rule.common.error.internalServerError")
-}
-
 func (r *RegistryRuleError) ArtifactNotFoundError(artifactID string) error {
 	return r.Localizer.MustLocalizeError("registry.rule.common.error.artifactNotFound", localize.NewEntry("ID", artifactID))
 }
