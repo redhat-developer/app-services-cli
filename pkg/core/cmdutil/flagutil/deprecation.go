eb1386cc02c3953f1d5c48be7408103803175c2e
Wojciech Trocki <wtrocki@redhat.com>
2022-02-17 14:32:10 +0000

    fix: remove unused deprecation.go

--- pkg/core/cmdutil/flagutil/deprecation.go
+++
@@ -1,6 +0,0 @@
-package flagutil
-
-// DeprecateFlag provides a way to deprecate a flag by appending standard prefixes to the flag description.
-func DeprecateFlag(flagDescription string) string {
-	return "DEPRECATED: " + flagDescription
-}
