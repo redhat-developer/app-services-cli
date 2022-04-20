897b4f80acc2f971a320998d458ebe5553c75014
Ramakrishna Pattnaik <rkpattnaik780@gmail.com>
2022-04-20 17:41:16 +0530

    fix(connector namespace): add validation

---
+++ pkg/cmd/connector/connectorcmdutil/validators.go
@@ -0,0 +1,28 @@
+package connectorcmdutil
+
+import (
+	"regexp"
+
+	"github.com/redhat-developer/app-services-cli/pkg/core/localize"
+)
+
+const (
+	legalNamespaceChars = "^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$"
+)
+
+// Validator is a type for validating connector attributes
+type Validator struct {
+	Localizer localize.Localizer
+}
+
+// ValidateNamespace validates the name of the namespace
+func (v *Validator) ValidateNamespace(name string) error {
+
+	matched, _ := regexp.Match(legalNamespaceChars, []byte(name))
+
+	if matched {
+		return nil
+	}
+
+	return v.Localizer.MustLocalizeError("connector.common.validation.namespace.error.invalidChars", localize.NewEntry("Namespace", name))
+}
