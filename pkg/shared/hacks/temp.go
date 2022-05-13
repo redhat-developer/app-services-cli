03c79f4dcf3fdd65fcca5984e8e6b9366424177a
Wojciech Trocki <wtrocki@redhat.com>
2022-05-13 16:37:51 +0100

    fix: add extra handling for empty response

---
+++ pkg/shared/hacks/temp.go
@@ -0,0 +1,56 @@
+package hacks
+
+// Temporary hack package
+// Nothing to see here
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	kafkamgmtclient "github.com/redhat-developer/app-services-sdk-go/kafkamgmt/apiv1/client"
+)
+
+// Temporary hack that we use to determine if
+// Our CLI needs to use mas-sso token
+func ShouldUseMasSSO() bool {
+	req, err := http.NewRequest("GET", "https://api.openshift.com/api/kafkas_mgmt/v1/sso_providers", nil)
+	if err != nil {
+		return true
+	}
+
+	req = req.WithContext(context.Background())
+
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return true
+	}
+
+	defer resp.Body.Close()
+
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return true
+	}
+
+	response := string(b)
+
+	// defining a struct instance
+	var provider *kafkamgmtclient.SsoProvider
+
+	responseBytes := []byte(fmt.Sprintf("%v", response))
+	err = json.Unmarshal(responseBytes, &provider)
+	if err != nil {
+		return true
+	}
+
+	if provider.GetBaseUrl() == "" {
+		return true
+	}
+
+	return provider.GetBaseUrl() == "https://identity.api.redhat.com"
+}
