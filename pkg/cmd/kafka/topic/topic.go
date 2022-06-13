f4782809ab93f96798318f70feda894c51ffb38c
JackDelahunt <jdealhunt@gmail.com>
2022-06-13 10:49:12 +0100

    feat: new consume command

--- pkg/cmd/kafka/topic/topic.go
+++ pkg/cmd/kafka/topic/topic.go
@@ -1,6 +1,7 @@
 package topic
 
 import (
+	"github.com/redhat-developer/app-services-cli/pkg/cmd/kafka/topic/consume"
 	"github.com/redhat-developer/app-services-cli/pkg/cmd/kafka/topic/create"
 	"github.com/redhat-developer/app-services-cli/pkg/cmd/kafka/topic/delete"
 	"github.com/redhat-developer/app-services-cli/pkg/cmd/kafka/topic/describe"
@@ -32,6 +33,7 @@ func NewTopicCommand(f *factory.Factory) *cobra.Command {
 		describe.NewDescribeTopicCommand(f),
 		update.NewUpdateTopicCommand(f),
 		produce.NewProduceTopicCommand(f),
+		consume.NewConsumeTopicCommand(f),
 	)
 
 	return cmd
