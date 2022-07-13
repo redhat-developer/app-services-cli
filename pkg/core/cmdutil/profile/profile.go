4ad0a0f24356825c80325b253aba6cf99f165000
Wojciech Trocki <wtrocki@redhat.com>
2022-07-13 13:20:06 +0100

    fix: remove dev preview env

--- pkg/core/cmdutil/profile/profile.go
+++ pkg/core/cmdutil/profile/profile.go
@@ -2,13 +2,8 @@
 package profile
 
 import (
-	"os"
-	"strconv"
-
 	"github.com/spf13/cobra"
 )
-
-const DevPreviewEnv = "RHOAS_DEV"
 
 // ApplyDevPreviewLabel adds visual element displayed in help
 func ApplyDevPreviewLabel(cmd *cobra.Command) {
@@ -23,16 +18,4 @@ func ApplyDevPreviewLabel(cmd *cobra.Command) {
 // DevPreviewAnnotation used in templates and tools like documentation generation
 func DevPreviewAnnotation() map[string]string {
 	return map[string]string{"channel": "preview"}
-}
-
-// DevModeEnabled Check if development mode is enabled
-func DevModeEnabled() bool {
-	rawEnvVal := os.Getenv(DevPreviewEnv)
-
-	boolVal, err := strconv.ParseBool(rawEnvVal)
-	if err != nil {
-		return false
-	}
-
-	return boolVal
 }
