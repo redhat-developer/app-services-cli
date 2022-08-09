6716a529c0434ba35445c6ac33a7d77118d5703a
Wojciech Trocki <wtrocki@redhat.com>
2022-08-09 21:07:33 +0100

    fix: alternative idea

--- pkg/cmd/connector/update/update.go
+++ pkg/cmd/connector/update/update.go
@@ -2,24 +2,19 @@ package update
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/jackdelahunt/survey-json-schema/pkg/surveyjson"
+
 	"github.com/redhat-developer/app-services-cli/pkg/core/cmdutil/flagutil"
-	"github.com/redhat-developer/app-services-cli/pkg/shared/connection"
-	"github.com/redhat-developer/app-services-cli/pkg/shared/connection/api"
 	"github.com/redhat-developer/app-services-cli/pkg/shared/connectorutil"
 	"github.com/redhat-developer/app-services-cli/pkg/shared/contextutil"
-	"github.com/redhat-developer/app-services-cli/pkg/shared/kafkautil"
 	"github.com/spf13/cobra"
-	"os"
 
 	"github.com/redhat-developer/app-services-cli/pkg/core/ioutil/dump"
+	"github.com/redhat-developer/app-services-cli/pkg/core/ioutil/editor"
 	"github.com/redhat-developer/app-services-cli/pkg/core/localize"
+	connectormgmtclient "github.com/redhat-developer/app-services-sdk-go/connectormgmt/apiv1/client"
 	connectorerror "github.com/redhat-developer/app-services-sdk-go/connectormgmt/apiv1/error"
-	"gopkg.in/AlecAivazis/survey.v1"
 
 	"github.com/redhat-developer/app-services-cli/pkg/shared/factory"
-	connectormgmtclient "github.com/redhat-developer/app-services-sdk-go/connectormgmt/apiv1/client"
 )
 
 type options struct {
@@ -75,20 +70,32 @@ func runUpdate(opts *options) error {
 		connector, _ = connectorutil.InteractiveSelect(conn, opts.f)
 	}
 
-	patch, err3 := setValuesInInteractiveMode(connector, opts, &conn)
-	if err3 != nil {
-		return err3
+	connectorChanged := false
+	if opts.name != "" {
+		connector.SetName(opts.name)
+		connectorChanged = true
+	}
+	if opts.namespaceID != "" {
+		connector.SetNamespaceId(opts.namespaceID)
+		connectorChanged = true
+	}
+	if opts.kafkaID != "" {
+		connector.Kafka.SetId(opts.kafkaID)
+		connectorChanged = true
 	}
 
-	connectorSpecification, err4 := askForConnectorDetails(&api, connector, opts)
-	if err4 != nil {
-		return err4
+	if !connectorChanged {
+		err := runEditor(connector)
+		if err != nil {
+			return err
+		}
 	}
-
-	patch["connector"] = connectorSpecification
+	var patchData map[string]interface{}
+	data, _ := json.Marshal(connector)
+	json.Unmarshal(data, &patchData)
 
 	a := api.ConnectorsMgmt().ConnectorsApi.PatchConnector(opts.f.Context, connector.GetId())
-	a = a.Body(patch)
+	a = a.Body(patchData)
 	updated, httpRes, err := a.Execute()
 	if httpRes != nil {
 		defer httpRes.Body.Close()
@@ -111,114 +118,21 @@ func runUpdate(opts *options) error {
 	return nil
 }
 
-func setValuesInInteractiveMode(connector *connectormgmtclient.Connector, opts *options, conn *connection.Connection) (map[string]interface{}, error) {
-
-	patch := make(map[string]interface{})
-
-	if opts.name == "" {
-		name, err := askForValue("Name", connector.GetName(), opts)
-		if err != nil {
-			return nil, err
-		}
-
-		opts.name = name
-	}
-	patch["name"] = opts.name
-
-	if opts.kafkaID == "" {
-		if opts.f.IOStreams.CanPrompt() {
-			kafka, err := kafkautil.InteractiveSelect(opts.f.Context, *conn, opts.f.Logger, opts.f.Localizer)
-			if err != nil {
-				return nil, err
-			}
-
-			patch["kafka"] = *kafka
-		} else {
-			return nil, opts.f.Localizer.MustLocalizeError("connector.interactive.error", localize.NewEntry("Field", "kafka.id"))
-		}
-	} else {
-		kafka, _, err := kafkautil.GetKafkaByID(opts.f.Context, (*conn).API().KafkaMgmt(), opts.kafkaID)
-		if err != nil {
-			return nil, err
-		}
-
-		patch["kafka"] = *kafka
-	}
-
-	if opts.namespaceID == "" {
-		namespaceId, err := askForValue("Namespace ID", connector.GetNamespaceId(), opts)
-		if err != nil {
-			return nil, err
-		}
-		opts.namespaceID = namespaceId
-	}
-
-	patch["namespace_id"] = opts.namespaceID
-
-	for key, value := range connector.Connector {
-		result, err := askForValue(key, fmt.Sprint(value), opts)
-		if err != nil {
-			return nil, err
-		}
-
-		connector.Connector[key] = result
-	}
-	patch["connector"] = connector.Connector
-
-	return patch, nil
-}
-
-func askForValue(field string, current string, opts *options) (string, error) {
-
-	if !opts.f.IOStreams.CanPrompt() {
-		return "", opts.f.Localizer.MustLocalizeError("connector.interactive.error", localize.NewEntry("Field", field))
-	}
-
-	var value string
-	prompt := &survey.Input{
-		Message: field,
-		Default: current,
-	}
-	err := survey.AskOne(prompt, &value, nil)
+func runEditor(currentMetadata *connectormgmtclient.Connector) error {
+	metadataJson, err := json.MarshalIndent(currentMetadata, "", " ")
 	if err != nil {
-		return value, err
+		return err
 	}
-	return value, nil
-}
-
-func askForConnectorDetails(api *api.API, connector *connectormgmtclient.Connector, opts *options) (*map[string]interface{}, error) {
-	response, _, err := (*api).ConnectorsMgmt().ConnectorTypesApi.GetConnectorTypeByID(opts.f.Context, connector.GetConnectorTypeId()).Execute()
+	systemEditor := editor.New(metadataJson, "metadata.json")
+	output, err := systemEditor.Run()
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	schemaOptions := surveyjson.JSONSchemaOptions{
-		Out:                 os.Stdout,
-		In:                  os.Stdin,
-		OutErr:              os.Stderr,
-		AskExisting:         true,
-		AutoAcceptDefaults:  false,
-		NoAsk:               false,
-		IgnoreMissingValues: false,
-	}
-
-	initialValues := make(map[string]interface{})
-
-	schemaBytes, err := json.Marshal(response.Schema)
+	var resultData *connectormgmtclient.Connector
+	err = json.Unmarshal(output, &resultData)
 	if err != nil {
-		return nil, err
+		return err
 	}
-
-	connectorValues, err := schemaOptions.GenerateValues(schemaBytes, initialValues)
-	if err != nil {
-		return nil, err
-	}
-
-	var connectorSpecification map[string]interface{}
-	err = json.Unmarshal(connectorValues, &connectorSpecification)
-	if err != nil {
-		return nil, err
-	}
-
-	return &connectorSpecification, nil
+	currentMetadata = resultData
+	return nil
 }
