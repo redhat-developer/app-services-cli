7f912534907a64fd48f30bd479e7c69466af8ee2
Wojciech Trocki <wtrocki@redhat.com>
2022-09-19 22:36:12 +0100

    chore: documentation generation

--- pkg/cmd/kafka/providers/providers.go
+++ pkg/cmd/kafka/providers/providers.go
@@ -28,7 +28,7 @@ type options struct {
 // NewListCommand creates a new command for listing kafkas.
 func NewProviderCommand(f *factory.Factory) *cobra.Command {
 	opts := &options{
-		ServiceContext: f.ServiceContext,
+		f: f,
 	}
 
 	cmd := &cobra.Command{
